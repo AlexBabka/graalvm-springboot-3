2ba6f30f7ac540e94415afb881915804f10b8663
Oleksii Babka <oleksii.babka@payconiq.com>
2023-03-21 14:06:37 +0100

    basic project structure

---
+++ src/main/java/com/alexbabka/graalvm/demo/GraalvmDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alexbabka.graalvm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexbabka/graalvm/demo/GraalvmDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbabka.graalvm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
